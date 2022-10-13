cce39c8a80a80b7597ae56ba3cf2a1e2d980f39a
Hung <aohhung@gmail.com>
2022-10-13 02:04:49 +0000

    'useIsMounted'

--- src/hooks/useIsMounted.ts
+++ src/hooks/useIsMounted.ts
@@ -2,12 +2,12 @@ import { MutableRefObject, useRef, useLayoutEffect } from 'react';
 
 export function useIsMounted():Readonly<MutableRefObject<boolean>>{
     const isMounted = useRef(false);
-    
+
     useLayoutEffect(()=>{
         isMounted.current = true;
 
-        return ()=> {
-            isMounted.current=false;
+        return () => {
+            isMounted.current = false;
         }
     },[]);
 
