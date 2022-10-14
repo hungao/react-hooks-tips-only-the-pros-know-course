07fad1bb9916f66a9ce5aed53036a4d75691366e
Hung <aohhung@gmail.com>
2022-10-14 10:11:05 +0000

    useWillUnmount

--- src/person-editor/usePerson.ts
+++ src/person-editor/usePerson.ts
@@ -4,50 +4,52 @@ import { sleep } from '../utils';
 
 import type { Person } from '../types/person';
 import { useIsMounted } from '../hooks/useIsMounted';
-import { useDebounce } from  '../hooks/useDebounce';
+import { useDebounce } from '../hooks/useDebounce';
+import { useWillUnmount } from '../hooks/useWillUnmount';
 
-function savePerson(person:Person|null):void{
-    console.log(`Saving person: ${JSON.stringify(person)}`);
-    localforage.setItem('person',person);
+function savePerson(person: Person | null): void {
+  console.log(`Saving person: ${JSON.stringify(person)}`);
+  localforage.setItem('person', person);
 }
 
-export function usePerson(initialPerson: Person){
-    console.log(`exec usePerson()`);
-    const [person, setPerson] = useState<Person | null>(null);
-    const isMounted = useIsMounted();
+export function usePerson(initialPerson: Person) {
+  console.log(`exec usePerson()`);
+  const [person, setPerson] = useState<Person | null>(null);
+  const isMounted = useIsMounted();
 
-    useEffect(()=>{
-      const getPerson = async () => {
-        const person = await localforage.getItem<Person>("person");
-        // console.log('sleeping');
-        // await sleep(3000);
-        // console.log('after sleeping');
-        if(isMounted.current){
-            setPerson(person ?? initialPerson);
-        }else{
-            console.log('not mounted');
-        }
-      };
-      getPerson();
-    },[initialPerson, isMounted]);
+  useEffect(() => {
+    const getPerson = async () => {
+      const person = await localforage.getItem<Person>("person");
+      // console.log('sleeping');
+      // await sleep(3000);
+      // console.log('after sleeping');
+      if (isMounted.current) {
+        setPerson(person ?? initialPerson);
+      } else {
+        console.log('not mounted');
+      }
+    };
+    getPerson();
+  }, [initialPerson, isMounted]);
 
-    const [, setNow] = useState(new Date());
-    useEffect(() => { 
-      const handle = setInterval(() => {
-        const t = new Date();
-        console.log(`=> usePerson state changed!`)
-        setNow(t);
-      }, 5000);
+  const [, setNow] = useState(new Date());
+  useEffect(() => {
+    const handle = setInterval(() => {
+      const t = new Date();
+      console.log(`=> usePerson state changed!`)
+      setNow(t);
+    }, 5000);
 
-      return () => clearInterval(handle);
-    }, []);
+    return () => clearInterval(handle);
+  }, []);
 
-    const saveFn = useCallback(() => { 
-      console.log(`useCallback`);
-      savePerson(person); 
-    }, [person]);
-  
-    useDebounce(saveFn, 1000);
-    
-    return [person, setPerson] as const;
+  const saveFn = useCallback(() => {
+    console.log(`useCallback`);
+    savePerson(person);
+  }, [person]);
+
+  useDebounce(saveFn, 1000);
+  useWillUnmount(saveFn);
+
+  return [person, setPerson] as const;
 }
